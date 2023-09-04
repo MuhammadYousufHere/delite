94fdc559cf43aa25e8ced5e141db5fe1eddf049a
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-09-04 20:46:44 +0500

    wip: app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,49 @@
 import 'dotenv/config'
+// import { graphqlUploadExpress } from 'graphql-upload'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 const PORT = process.env.PORT
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
+
+  app.startAllMicroservices()
+
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: false
+    })
+  )
+  // Swagger Api
+  // if (swaggerConfig.enabled) {
+  //   const options = new DocumentBuilder()
+  //     .setTitle(swaggerConfig.title || 'Nestjs')
+  //     .setDescription(swaggerConfig.description || 'The nestjs API description')
+  //     .setVersion(swaggerConfig.version || '1.0')
+  //     .build()
+  //   const document = SwaggerModule.createDocument(app, options)
+
+  //   SwaggerModule.setup(swaggerConfig.path || 'api', app, document)
+  // }
+  // enable shutdown hook
+  app.enableShutdownHooks()
+  // Cors
+  if (process.env.CORS_ENABLE === '1') {
+    app.enableCors()
+  }
+
   await app.listen(PORT)
 }
-bootstrap()
+
+bootstrap().catch((error) => {
+  // new Logger({ component: SERVICE_NAME, isProduction: true }).error(
+  //   error.message,
+  //   error
+  // )
+  console.log(error)
+  process.exit(1)
+})
