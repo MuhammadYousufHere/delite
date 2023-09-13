602c14b333569eb794ede3fb6738f42438fe3cae
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-09-13 23:46:01 +0500

    app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ConfigModule } from '@nestjs/config'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { AppService } from './app.service'
+import { AppController } from './app.controller'
+import { AppResolver } from './app.resolver'
+import config from './common/configs/config'
+import { GqlConfigService } from './gql-config-service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver]
 })
 export class AppModule {}
